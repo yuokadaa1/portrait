726aebc0105a23a7a828360df1d76bad2c125827
yuu oka <yuu.okd.hotmail@gmail.com>
2020-01-11 16:58:04 +0900

    a

--- routes/api.php
+++ routes/api.php
@@ -29,10 +29,6 @@ use App\Post;
    //dBから情報を抽出して取得
   $Post = Post::select('modelId','modelIdNum','kbnId','folderPath','created_at')->orderBy('kbnId', 'asc')->orderBy('created_at', 'desc')->first()->toArray();
 
-  //assetでの読み込み=public => public/storageにデータを格納、呼び出しが必要。
-  dd(base64_encode(file_get_contents(asset('storage/ninja_woman_face3_sad.png'))));
-  dd(file_get_contents(asset('storage/photo1_1.png')));
-
   $responseBody = array();
   foreach ($Post as & $value) {
     $httpResponse = array();
@@ -42,10 +38,8 @@ use App\Post;
     $httpResponse['kbnId'] = $value['kbnId'];
     // $httpResponse['kbnName'] = $value['kbnName'];
     $httpResponse['images'] = file_get_contents(asset($value['folderPath']));
-    // $httpResponse['images'] = file_get_contents(asset('images/ninja_woman_face1_smile.png'));
-    // dd($httpResponse);
     array_push($responseBody,$httpResponse);
-    // $responseBody.push($httpResponse);
+    $responseBody.push($httpResponse);
   }
 
   // 読み込み自体はできた->これでencodeされるのは画像のURL
@@ -54,19 +48,15 @@ use App\Post;
   //　これで「public\images\ninja_woman_face1_smile.png」を読み込む。
   // $gazo = base64_encode(file_get_contents(asset('images/ninja_woman_face1_smile.png')));
 
+  //assetでの読み込み=public => public/storageにデータを格納、呼び出しが必要。
+  // dd(base64_encode(file_get_contents(asset('storage/ninja_woman_face3_sad.png'))));
+
   $responseHeaders = [
    "X-Pages" => 1
   ];
-  // $responseBody = $Post->toArray();
-  // $responseBody = [
-  //  "person" => array(
-  //   "性別" => "男性",
-  //   "血液型" => "A型",
-  //   "血液型" => "A型",
-  //   "画像" => $gazo
-  //  )
-  // ];
+
   $statusCode = 200;
+
   return response()->json($responseBody, $statusCode, $responseHeaders,JSON_UNESCAPED_UNICODE);
 
  });
